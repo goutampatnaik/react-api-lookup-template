989c68cc1745d8a328447275fcc1d3aa045ef5ea
goutam patnaik <goutam.nitr@gmail.com>
2020-07-14 15:20:27 +0530

    Updates

---
+++ precache-manifest.9c45b0558293fe4738697eb36c5d694e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b9610dfdb6fce73e997c5ed8cd19594",
+    "url": "/react-api-lookup-template/index.html"
+  },
+  {
+    "revision": "8127937e1dee02b72c11",
+    "url": "/react-api-lookup-template/static/css/main.f06f8caa.chunk.css"
+  },
+  {
+    "revision": "8d4d572b7ad34fbc82b3",
+    "url": "/react-api-lookup-template/static/js/2.d3a3e9f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-api-lookup-template/static/js/2.d3a3e9f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8127937e1dee02b72c11",
+    "url": "/react-api-lookup-template/static/js/main.c35cc786.chunk.js"
+  },
+  {
+    "revision": "aaae8b7366ac29d3258d",
+    "url": "/react-api-lookup-template/static/js/runtime-main.ec1a212c.js"
+  }
+]);
